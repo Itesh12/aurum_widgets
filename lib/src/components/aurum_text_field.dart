2e1590f7b7322fa57d0f32bf53a0c01c6b93577e
Your Name <yourname@gmail.con>
2026-04-21 18:42:04 +0530

    Cleanup: remove unused GetX import in AurumTextField

--- lib/src/components/aurum_text_field.dart
+++ lib/src/components/aurum_text_field.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
-import "package:get/get.dart";
 import "../utils/aurum_regex.dart";
 import "../utils/aurum_assets.dart";
 
